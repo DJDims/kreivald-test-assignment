d659eb210632b41b88ecdd092de00245185b8152
DJDims <djdims123@mail.ru>
2024-12-05 14:37:33 +0200

    Unique positions bugfix

--- database/factories/PositionFactory.php
+++ database/factories/PositionFactory.php
@@ -9,18 +9,9 @@ class PositionFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->randomPosition(),
+            'name' => $this->faker->unique()->jobTitle(),
             'created_at' => now(),
             'updated_at' => now(),
         ];
-    }
-
-    private function randomPosition(): string
-    {
-        $prefixes = ['Junior', 'Senior', 'Lead', 'Chief', 'Assistant', 'Executive'];
-        $roles = ['Developer', 'Manager', 'Designer', 'Analyst', 'Engineer', 'Coordinator'];
-        $prefix = $this->faker->randomElement($prefixes);
-        $role = $this->faker->randomElement($roles);
-        return $prefix . ' ' . $role;
     }
 }
